d8c3a682b621e120739502f4f2bf2c79fe2dec6f
Cyb3rmancer <61503962+Cyb3rmancer@users.noreply.github.com>
2020-02-27 14:33:25 +0100

    Update init.js

--- mcdowell/init.js
+++ mcdowell/init.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     setTimeout(function() {
 
       //testing in real time
-      alert("testing in real time, afternoon");
+      alert("testing in real time, change");
       
     }, 300);
 });
