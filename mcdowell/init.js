7425ab1f83ebd6860b41d374e7d03852301e6316
developer-evie <61504519+developer-evie@users.noreply.github.com>
2020-02-28 08:34:10 +0100

    Update init.js

--- mcdowell/init.js
+++ mcdowell/init.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     setTimeout(function() {
 
       //testing in real time
-      alert("testing in real time, five minutes test");
+      alert("testing in real time, five minutes test 123");
       
     }, 300);
 });
